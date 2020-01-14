dd62c5448006e882d7dde0faa2f710be327f7ecf
Ryan <ryanwevans@icloud.com>
2020-01-13 19:08:30 -0800

    Add devise; add devise user model

--- NOTES.md
+++ NOTES.md
@@ -6,8 +6,10 @@ Models:
         dose - integer
         date - string
         time - integer
-        
+
     User (has many records)
         name - string
         email - string
         password_digest - string
+
+Add Devise for authentication
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    before_action :require_log_in
+    # protect_from_forgery with: :exception
+    # before_action :require_log_in
 
     helper_method :current_user
     helper_method :logged_in?
