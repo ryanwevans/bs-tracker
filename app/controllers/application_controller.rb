7840249fd0699ab6012334e9c19f3fbd145d1675
Ryan <ryanwevans@icloud.com>
2020-01-13 19:26:41 -0800

    Remove sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,21 @@ class ApplicationController < ActionController::Base
     # protect_from_forgery with: :exception
     # before_action :require_log_in
 
-    helper_method :current_user
-    helper_method :logged_in?
+    # helper_method :current_user
+    # helper_method :logged_in?
 
-    def logged_in?
-        !!current_user
-    end
+    # def logged_in?
+    #     !!current_user
+    # end
 
 
-    private
+    # private
 
-    def require_log_in
-        redirect_to root_path unless logged_in?
-    end
+    # def require_log_in
+    #     redirect_to root_path unless logged_in?
+    # end
 
-    def current_user
-        @current_user ||= User.find_or_create_by(id: session[:user_id]) if session[:user_id]
-    end
+    # def current_user
+    #     @current_user ||= User.find_or_create_by(id: session[:user_id]) if session[:user_id]
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'sessions#new'
 
   resources :records
 
-  get '/signin' => 'sessions#new'
-  post '/signin' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
 end
