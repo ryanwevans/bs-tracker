1b8c1bad7fd4f3b83854288c2d6679e6df46e535
Ryan <ryanwevans@icloud.com>
2020-01-28 21:16:44 -0800

    Update Record#sort to also sort time

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,6 +2,7 @@ class Record < ApplicationRecord
     belongs_to :user
 
     def self.sorted_desc
-        order(date: :desc)
+        order(date: :desc, time: :desc)
+        # sort_by{ |t| [t.date, t.time] }
     end
 end
