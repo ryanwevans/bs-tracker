6f8b9736e7472bf37acef9bbb61d1aa8dd89363a
Ryan <ryanwevans@icloud.com>
2020-01-29 11:21:35 -0800

    Add login/out to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'records#index'
+  unauthenticated do
+    root :to => 'sessions#new'
+  end
+  
+  authenticated do
+    root :to => 'records#index'
+  end
+
+  # root 'records#index'
 
   resources :records
   
